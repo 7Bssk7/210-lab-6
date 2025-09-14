c9fc3b39aa3b35facd608769b9d8815d5e330ec9
7Bssk7 <arhip.fin3@gmail.com>
2025-09-14 13:36:53 -0700

    Update

--- 210-lab-6.cpp
+++ 210-lab-6.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 using namespace std;
 
+const double SIZE = 5;
+
+double* enterArrayData();
+void outputArrayData(double *arr);
+
 int main(){
+    double *listArr = nullptr;
+    listArr = new double[SIZE];
+    listArr = enterArrayData();
+    outputArrayData(listArr);
+    delete [] listArr;
 
     return 0;
 }
+
+double* enterArrayData(){
+    double temp;
+    double *arr = nullptr;
+    arr = new double[SIZE];
+    for(int i = 0; i < SIZE; ++i){
+        cin >> temp;
+        arr[i] = temp;
+    }
+    return arr;
+}
+
+void outputArrayData(double *arr){
+    for(int i = 0; i < SIZE; ++i){
+        cout << "Element #" << i+1 << arr[i] << endl;
+    }
+}
+
+
