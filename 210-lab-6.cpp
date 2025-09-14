e680900e462c46d962572d8ff6993ee0e32037f4
7Bssk7 <arhip.fin3@gmail.com>
2025-09-14 14:04:18 -0700

    Update

--- 210-lab-6.cpp
+++ 210-lab-6.cpp
@@ -4,29 +4,29 @@ using namespace std;
 
 const int SIZE = 5;
 
-double* enterArrayData();
+double* enterArrayData(double *list);
 double sumArray(double *arr);
 void outputArrayData(double *arr);
 
 int main(){
+    double *arr = nullptr;
+    arr = new double[SIZE] {5.5, 2.6, 3.4, 8.4, 9.2};
     double *listArr = nullptr;
     listArr = new double[SIZE];
-    listArr = enterArrayData();
+    listArr = enterArrayData(arr);
     outputArrayData(listArr);
     delete [] listArr;
 
     return 0;
 }
 
-double* enterArrayData(){
-    double temp;
+double* enterArrayData(double *list){
     double *arr = nullptr;
     arr = new double[SIZE];
+    arr = list;
     cout << "Data entry for the array:" << endl;
     for(int i = 0; i < SIZE; ++i){
-        cout  << "    > Element #" << i << ": ";
-        cin >> temp;
-        arr[i] = temp;
+        cout  << "    > Element #" << i << ": " << arr[i] << endl;
     }
     cout << "Data entry complete." << endl;
     return arr;
